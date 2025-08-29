2e68dc6abcf9e1bac330e51d2981694885f877e2
Kavin Venthan Ravi Sudha <kavinventhan12@gmail.com>
2025-08-29 15:01:37 +0530

    added urls of home

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    obj={
+        'name':'kavin',
+        'age':21,
+        'place':'chennai'
+    }
+    return Response(obj)
